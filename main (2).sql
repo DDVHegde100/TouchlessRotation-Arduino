04f1ea9e65490114ba3be6d1adb2c49ad792c3b9
DDVHegde100 <69629876+DDVHegde100@users.noreply.github.com>
2021-07-30 10:21:22 -0400

    Add files via upload

---
+++ main (2).sql
@@ -0,0 +1,10 @@
+create table book_details (book_name varchar2(100), 
+author_name varchar2(100), 
+price float, 
+
+
+insert into book_details values ("The Bad Beginning", "Lemony Snicket",6.99);
+insert into book_details values ("The Reptile Room", "Lemony Snicket",7.99);
+insert into book_details values ("The Wide Window", "Lemony Snicket",6.99);
+insert into book_details values ("The Miserable Mill", "Lemony Snicket",13.99);
+insert into book_details values ("The Austere Academy", "Lemony Snicket",12.99);
